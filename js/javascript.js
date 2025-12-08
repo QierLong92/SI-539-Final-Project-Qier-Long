ef07d4f1e442967bdc6199790684f92c3aab568f
Qier Long <longqi@umich.edu>
2025-12-07 23:47:07 -0500

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+
     const buttons = document.querySelectorAll(".season-btn");
-    const panels = document.querySelectorAll(".season-panel");
+    const panels = document.querySelectorAll(".season_panel_active");
 
     buttons.forEach(btn => {
         btn.addEventListener("click", () => {
@@ -13,6 +13,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
             panels.forEach(panel => panel.classList.remove("active"));
             document.getElementById(season).classList.add("active");
+            console.log(`Switched to ${season} panel`);
         });
+
     });
+
+    const infoBox = document.getElementById("infoBox");
+    const infoTitle = document.getElementById("info-title");
+    const infoText = document.getElementById("info-text");
+
+    // Button 4
+    const btn4 = document.querySelector(".map-btn btn4");
+    btn4.addEventListener("click", () => {
+        infoTitle.textContent = "South Michigan";
+        infoText.textContent = "Suburban and urban areas with a mix of residential and commercial properties. Common pests include ants, spiders, and rodents.";
+        infoBox.style.display = "block";
+        console.log("Button 4 clicked");
+    });
+
+
+
 });
